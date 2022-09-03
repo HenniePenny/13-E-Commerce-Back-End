5f260d072833bb73cae74c6ca12a7affb32161aa
HenniePenny <h.kressner@hotmail.de>
2022-09-03 16:03:34 +0200

    add categoryID route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,9 +16,23 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryID = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!categoryID) {
+      res
+        .status(404)
+        .json({ message: "Sorry, no category found with that ID." });
+      return;
+    }
+    res.status(200).json(categoryID);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.post("/", (req, res) => {
